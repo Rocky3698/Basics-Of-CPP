32e634a6849467b7868587f104ba421f43e2d1dc
Rocky Chowdhury <rocky20809@gmail.com>
2023-05-29 21:00:48 +0600

    Edited

--- Array/Insertion_Element.cpp
+++ Array/Insertion_Element.cpp
@@ -7,7 +7,22 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-
+    int index, value, size;
+    cin >> size >> index >> value;
+    int arr[size + 1];
+    for (int i = 0; i < size; i++) // input
+    {
+        cin >> arr[i];
+    }
+    for (int i = size; i >= index; i--) // position changing
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[index] = value;            // inserting
+    for (int i = 0; i < size+1; i++) // output
+    {
+        cout << arr[i]<<" ";
+    }
 
     return 0;
 }
